fb2e820d82a6c29cd7a24f76c53716717c46857a
erko <erko@risthein.ee>
2015-11-10 14:02:56 +0000

    implemnted the first scenario that lets you add director info to an existing movie

--- rottenpotatoes/spec/controllers/movies_controller_spec.rb
+++ rottenpotatoes/spec/controllers/movies_controller_spec.rb
@@ -3,12 +3,6 @@ require 'rails_helper'
 describe MoviesController do
     
   describe 'GET index' do
- #   it 'assigns @teams' do
- #     team = Team.create
- #     get :index
- #     expect(assigns(:teams)).to eq([team])
- #   end
- 
     it 'responds successfully with an HTTP 200 status code' do
       get :index
       expect(response).to be_success
@@ -25,6 +19,16 @@ describe MoviesController do
       terminator = Movie.create(:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984', :director => 'James Cameron')
       get :index
       expect(assigns(:movies)).to match_array([aladdin, terminator])
+    end
+  end
+  
+  describe 'POST update' do
+    it 'successfully updates the director of a movie' do
+      aladdin = Movie.create(:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992', :director => 'Unknown')
+      post :update, :id => aladdin.id, :movie => {:director => 'Ridley Scott'}
+      aladdin.reload
+      expect(aladdin.director).to eq('Ridley Scott')
+      expect(response).to redirect_to(movie_path(aladdin))
     end
   end
 
