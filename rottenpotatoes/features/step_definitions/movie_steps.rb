e6e9a1132b2bac0e9d2a259c21ddc78fbcef207e
erko <erko@risthein.ee>
2015-11-10 11:57:54 +0000

    added simplecov and generated some specs

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,8 +1,10 @@
-Given(/^the following movies exist:$/) do |table|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+Given(/^the following movies exist:$/) do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create(movie)
+  end
 end
 
-Then(/^the director of "(.*?)" should be "(.*?)"$/) do |arg1, arg2|
+
+Then(/^the director of "(.*?)" should be "(.*?)"$/) do |movie, director|
   pending # express the regexp above with the code you wish you had
 end
